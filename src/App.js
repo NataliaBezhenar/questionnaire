0207c752c66191a95a7ff9d8ea20d3d60c426b8b
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2022-07-21 16:43:47 +0200

    initial project setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+//import Form from "./components/Form/Form";
+
+class App extends Component {
+  formSubmitHandler = (data) => {
+    console.log(data);
+  };
+
+  render() {
+    return <div></div>;
+
+    //   <Form abc={this.formSubmitHandler} />;
+    // }
+  }
+}
+
+export default App;
