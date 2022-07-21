291ef09b7f7d5b5769249984b59b57a61a50bb4b
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2022-07-21 22:13:05 +0200

    header added, styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Component } from "react";
+import Container from "./components/Container/Container";
 import Form from "./components/Form/Form";
+import Header from "./components/Header/Header";
 
 class App extends Component {
   formSubmitHandler = (data) => {
@@ -7,7 +9,12 @@ class App extends Component {
   };
 
   render() {
-    return <Form abc={this.formSubmitHandler} />;
+    return (
+      <Container>
+        <Header />
+        <Form abc={this.formSubmitHandler} />
+      </Container>
+    );
   }
 }
 
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -45,130 +45,144 @@ class Form extends Component {
 
   render() {
     return (
-      <form action="" onSubmit={this.handleSubmit} onReset={this.resetForm}>
-        <div className="input-field">
-          <label>
-            First Name
-            <input
-              type="text"
-              name="firstName"
-              maxLength="50"
-              minLength="2"
-              value={this.state.firstName}
-              onChange={this.handleChange}
-              placeholder="John"
-              required
-            />
-          </label>
+      <form
+        className="form"
+        onSubmit={this.handleSubmit}
+        onReset={this.resetForm}
+      >
+        <div className="form__inputs-group">
+          <div className="input-field">
+            <label>
+              First Name
+              <input
+                type="text"
+                name="firstName"
+                maxLength="50"
+                minLength="2"
+                value={this.state.firstName}
+                onChange={this.handleChange}
+                placeholder="John"
+                required
+              />
+            </label>
+          </div>
+
+          <div className="input-field">
+            <label>
+              Last Name
+              <input
+                type="text"
+                name="lastName"
+                maxLength="50"
+                minLength="2"
+                value={this.state.lastName}
+                onChange={this.handleChange}
+                placeholder="Johnson"
+                required
+              />
+            </label>
+          </div>
+
+          <div className="input-field">
+            <label>
+              Birth Date
+              <input
+                type="date"
+                min="1950-01-01"
+                max="2022-12-31"
+                name="birthDate"
+                value={this.state.birthDate}
+                onChange={this.handleChange}
+                required
+              />
+            </label>
+          </div>
+
+          <div className="input-field">
+            <label>
+              Phone number
+              <input
+                type="tel"
+                name="phone"
+                value={this.state.phone}
+                onChange={this.handleChange}
+                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                required
+                placeholder="123-123-1234"
+              />
+            </label>
+          </div>
+
+          <div className="input-field">
+            <label>
+              Your site
+              <input
+                type="text"
+                name="site"
+                maxLength="50"
+                value={this.state.site}
+                onChange={this.handleChange}
+                placeholder="https://developer.mozilla.org/"
+              />
+            </label>
+          </div>
         </div>
 
-        <div className="input-field">
-          <label>
-            Last Name
-            <input
-              type="text"
-              name="lastName"
-              maxLength="50"
-              minLength="2"
-              value={this.state.lastName}
-              onChange={this.handleChange}
-              placeholder="Johnson"
-              required
-            />
-          </label>
+        <div className="form__text-areas">
+          <div className="input-field">
+            <label>
+              About Yourself
+              <textarea
+                type="text"
+                name="about"
+                rows="7"
+                cols="50"
+                value={this.state.about}
+                onChange={this.handleChange}
+                placeholder="Few words about yourself..."
+              />
+            </label>
+          </div>
+
+          <div className="input-field">
+            <label>
+              Technologies Stack
+              <textarea
+                type="text"
+                name="technologies"
+                rows="7"
+                cols="50"
+                value={this.state.technologies}
+                onChange={this.handleChange}
+                placeholder="HTML5, CSS3, JavaScript..."
+              />
+            </label>
+          </div>
+
+          <div className="input-field">
+            <label>
+              Last Project Description
+              <textarea
+                type="text"
+                name="project"
+                rows="7"
+                cols="50"
+                value={this.state.project}
+                onChange={this.handleChange}
+                placeholder="Describe your last project please"
+              />
+            </label>
+          </div>
         </div>
 
-        <div className="input-field">
-          <label>
-            Birth Date
-            <input
-              type="date"
-              min="1950-01-01"
-              max="2022-12-31"
-              name="birthDate"
-              value={this.state.birthDate}
-              onChange={this.handleChange}
-              required
-            />
-          </label>
+        <div className="input__buttons-group">
+          <button className="btn btn-full" type="submit">
+            Save
+          </button>
+          <button className="btn btn-outline" type="reset">
+            Reset the form!
+          </button>
         </div>
-
-        <div className="input-field">
-          <label>
-            Phone number
-            <input
-              type="tel"
-              name="phone"
-              value={this.state.phone}
-              onChange={this.handleChange}
-              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-              required
-              placeholder="123-123-1234"
-            />
-          </label>
-        </div>
-
-        <div className="input-field">
-          <label>
-            Your site
-            <input
-              type="text"
-              name="site"
-              maxLength="50"
-              value={this.state.site}
-              onChange={this.handleChange}
-              placeholder="https://developer.mozilla.org/"
-            />
-          </label>
-        </div>
-
-        <div className="input-field">
-          <label>
-            About Yourself
-            <textarea
-              type="text"
-              name="about"
-              rows="7"
-              cols="50"
-              value={this.state.about}
-              onChange={this.handleChange}
-              placeholder="Few words about yourself..."
-            />
-          </label>
-        </div>
-
-        <div className="input-field">
-          <label>
-            Technologies Stack
-            <textarea
-              type="text"
-              name="technologies"
-              rows="7"
-              cols="50"
-              value={this.state.technologies}
-              onChange={this.handleChange}
-              placeholder="HTML5, CSS3, JavaScript..."
-            />
-          </label>
-        </div>
-
-        <div className="input-field">
-          <label>
-            Last Project Description
-            <textarea
-              type="text"
-              name="project"
-              rows="7"
-              cols="50"
-              value={this.state.project}
-              onChange={this.handleChange}
-              placeholder="Describe your last project please"
-            />
-          </label>
-        </div>
-
-        <button type="submit">Save</button>
-        <button type="reset">Reset the form!</button>
       </form>
     );
   }
